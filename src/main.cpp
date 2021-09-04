4a5ac7a5e63db56d7e492c593c362d37e5f9659d
snouragan <74413033+snouragan@users.noreply.github.com>
2021-09-04 15:26:37 -0700

    test
    serial communication working

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <Arduino.h>
 
 void setup() {
-  // put your setup code here, to run once:
+  // SERIAL:
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  Serial.println("alo");
+  delay(1000);
 }
